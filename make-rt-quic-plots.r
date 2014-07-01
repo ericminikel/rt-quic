e4daa8d2c68cc3785ddf638173cde89993ddfc97
Eric Minikel <ericminikel@gmail.com>
2014-06-30 18:23:40 -0600

    plots are working for hardcoded example

--- make-rt-quic-plots.r
+++ make-rt-quic-plots.r
@@ -33,29 +33,55 @@ get_dilution_color = function(dilution) {
 }
 
 mat = as.matrix(data[4:(dim(data)[2]-1)])
+# normalize the matrix
+mat = (mat - min(mat)) / (max(mat) - min(mat))
+
+# get the timepoints for the x axis
 timepts = extract_timepoints(colnames(mat))
+timepts_h = timepts/60
 
+metadata$dil_log10 = -log10(metadata$dilution)
+dil_range = range(metadata$dil_log10,na.rm=TRUE)
+desired_range = c(0,255*.8) # dilution colors will range from #000 to #CCC
+metadata$graylevel = round((metadata$dil_log10 - min(dil_range))/max(dil_range) * (max(desired_range) - min(desired_range)))
 
-technician = "eric"
+plotby = "technician"
+curveby = c("seed","dilution","comments")
 
-toplot = metadata$technician==technician & metadata$used
-sum(toplot)
-metadata$curve = paste(metadata$seed[toplot],metadata$dilution[toplot],sep=" ")
-curves = unique(metadata$curve)
-plot(NA,NA,xlim=range(timepts)*1.2,ylim=dynrange,
-     xlab='Timepoint', ylab='ThT fluorescence units',
-     main=paste('rtq00001',technician,sep="-"))
-for (curve in curves) {
-  userows = toplot & metadata$curve==curve
-  seed = unique(metadata$seed[userows]) # there should be only one
-  if (seed=="NBH") {
-    color = 'green'
-  } else {
-    color = get_dilution_color(unique(metadata$dilution[userows]))
+plotbyval="matteo"
+metadata$curve = do.call(paste,metadata[,curveby])
+legend = data.frame(name=character(),color=character())
+for (plotbyval in unique(metadata[metadata$used,plotby])) {
+  plot(NA,NA,xlim=range(timepts_h),ylim=c(0,1),
+       xlab='Timepoint', ylab='Relative ThT fluorescence units',
+       main=paste('rtq00001',plotbyval,sep="-"))
+  curvenames = unique(metadata$curve[metadata$used & metadata[,plotby]==plotbyval])
+  for (curvename in curvenames) {
+    # figure out which rows to average for this curve
+    userows = metadata$used & metadata[,plotby]==plotbyval & metadata$curve==curvename
+    # calculate the y values by averaging those rows
+    yvals = colMeans(mat[userows,])
+    # figure out what color to plot
+    current_seed = unique(metadata$seed[userows])
+    # figure out what dilution this is
+    dilution = unique(metadata$dilution[userows])
+    seed = unique(metadata$seed[userows])
+    graylevel = unique(metadata$graylevel[userows])
+    if (seed=="NBH") {
+      curve_hexcolor = "#00CC00" # green
+#      rgb_multiplier = c(0,1,0) # green scale
+    } else {
+      rgb_multiplier = c(1,1,1) # gray scale
+      curve_rgb = round(rgb_multiplier * graylevel)
+      curve_rgb[is.na(curve_rgb)] = 0 # fix in case there are NA
+      curve_hexcolor = paste("#",paste(sprintf("%02x",curve_rgb),collapse=""),sep="")
+    }
+    points(timepts_h,yvals,type='l',lwd=3,col=curve_hexcolor)
+#    text(timepts[length(timepts)],curvedata[length(curvedata)],label=curve,col=color,pos=4,cex=.8)
+    legend = rbind(legend,cbind(curvename,curve_hexcolor))
   }
-  curvedata = colMeans(mat[toplot & metadata$curve==curve,])
-  points(timepts,curvedata,type='l',lwd=3,col=color)
-  text(timepts[length(timepts)],curvedata[length(curvedata)],label=curve,col=color,pos=4,cex=.8)
 }
+legend('bottomright',legend[,1],col=legend[,2],lwd=2)
+
 
 
