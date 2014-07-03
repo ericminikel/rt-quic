7b587ccb5c592cd658911f552c37abb140569b07
Eric Minikel <ericminikel@gmail.com>
2014-07-03 15:15:33 -0600

    fixed bug; final concs are in 100 uL not 98 uL volume

--- master-mix-planner.r
+++ master-mix-planner.r
@@ -18,21 +18,25 @@ master_mix_planner = function (
     ) {
     pbs_nacl = 130 # concentration of NaCl in PBS in mM
     fudge_factor = 1 + excess
-    vol_total = (well_volume - bh_volume) * n_wells * fudge_factor
-    vol_pbs  = vol_total * final_pbs / input_pbs
-    vol_tht  = vol_total * final_tht / input_tht
-    vol_edta = vol_total * final_edta / input_edta
-    vol_rprp = vol_total * final_rprp / input_rprp
-    vol_nacl = vol_total * (final_nacl - pbs_nacl * final_pbs) / input_nacl
-    vol_water = vol_total - vol_pbs - vol_tht - vol_edta - vol_rprp - vol_nacl
+    vol_total_incl_bh = well_volume * n_wells * fudge_factor
+    vol_master_mix  = (well_volume - bh_volume) * n_wells * fudge_factor
+    vol_pbs  = vol_total_incl_bh * final_pbs / input_pbs
+    vol_tht  = vol_total_incl_bh * final_tht / input_tht
+    vol_edta = vol_total_incl_bh * final_edta / input_edta
+    vol_rprp = vol_total_incl_bh * final_rprp / input_rprp
+    vol_nacl = vol_total_incl_bh * (final_nacl - pbs_nacl * final_pbs) / input_nacl
+    vol_water = vol_master_mix - vol_pbs - vol_tht - vol_edta - vol_rprp - vol_nacl
     cat(paste("PBS ",input_pbs,"X: ",round(vol_pbs), "uL\n",sep=""))
     cat(paste("ThT ",input_tht,"uM: ",round(vol_tht), "uL\n",sep=""))
     cat(paste("EDTA ",input_edta,"mM: ",round(vol_edta), "uL\n",sep=""))
     cat(paste("NaCl ",input_nacl,"mM: ",round(vol_nacl), "uL\n",sep=""))
     cat(paste("rPrP ",input_rprp,"mg/mL: ",round(vol_rprp), "uL\n",sep=""))
     cat(paste("water: ",round(vol_water), "uL\n",sep=""))
-    cat(paste("TOTAL VOLUME: ",round(vol_total), "uL\n",sep=""))
+    cat(paste("TOTAL MASTER MIX VOLUME: ",round(vol_master_mix), "uL\n",sep=""))
 }
 
 # example usage:
 master_mix_planner(input_rprp=.6,n_wells=96)
+
+# validate that we get the same answer we got manually for rtq00003 
+master_mix_planner(n_wells=96,excess=.123,input_rprp=.6)
